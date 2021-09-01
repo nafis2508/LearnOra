cab293b70c87da661d28bc434b0ffb9803aa802f
nafis2508 <md.nafis08@gmail.com>
2021-09-01 20:59:16 +0600

    quiz newquestion.html

--- classroom/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from classroom.models import Course, Category
-
-class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title",)}
-
-admin.site.register(Course)
-admin.site.register(Category, CategoryAdmin)
-
