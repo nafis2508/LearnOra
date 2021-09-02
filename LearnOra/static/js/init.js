097563cd2c2725fc187737978833bf941e99b696
unknown <sadiqunnur.97@gmail.com>
2021-09-02 21:24:40 +0600

    classroom app completed

---
+++ LearnOra/static/js/init.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.tabs').tabs();
+    $('.materialboxed').materialbox();
+    $('select').formSelect();
+    $('.slider').slider();
+    $('.tooltipped').tooltip();
+    $('.modal').modal();
+
+    
+      }); // end of document ready
+})(jQuery); // end of jQuery name space
