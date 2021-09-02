56d5f036de387b095db67b45e9c1214a887e8b38
Faisal00001 <57837185+Faisal00001@users.noreply.github.com>
2021-09-03 02:30:58 +0600

    I have work on question app

--- LearnOra/urls.py
+++ LearnOra/urls.py
@@ -1,4 +1,4 @@
-"""LearnOra URL Configuration
+"""student_portal URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+from authy.views import UserProfile
+from classroom.views import index
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('user/', include('authy.urls')),
+    path('course/', include('classroom.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('direct/', include('direct.urls')),
+    path('<username>', UserProfile, name='profile'),
+    path('', index, name='index')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
